5a44cdf9b3e0814567064c84867cdf9c2812d7e7
unknown <aasim.sheik.988@gmail.com>
2022-08-11 15:35:29 +0200

    initial

---
+++ public/js/game.js
@@ -0,0 +1,17 @@
+boxNumbers();
+
+function boxNumbers() {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((box, i) => {
+    if (
+      String(i).length == 1 ||
+      (String(i).length == 2 && Number(String(i)[0])) % 2 == 0
+    ) {
+      box.innerHTML = `${100 - i}`;
+    } else {
+      box.innerHTML = String(
+        Number(`${9 - Number(String(i)[0])}${String(i)[1]}`) + 1
+      );
+    }
+  });
+}
